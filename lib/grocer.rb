0b9c1f88670c7e43bca14aea3f3658049a4d6dfb
Gabriel Demes <gabrieldemes@nyu.edu>
2021-01-03 01:06:26 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,12 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  collection.each { |item|
+    if item.has_value?(name)
+      return item
+    end
+  }
+  end
 
 end
 
